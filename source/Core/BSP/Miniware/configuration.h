700cd54b887bcbefd58ef50555525cde4706390d
karl <33448991+karlhh@users.noreply.github.com>
2024-03-04 11:40:23 +0100

    Update configuration.h

--- source/Core/BSP/Miniware/configuration.h
+++ source/Core/BSP/Miniware/configuration.h
@@ -253,7 +253,7 @@
 #define VOLTAGE_DIV              650  // Default for TS80P with slightly different resistors
 #define CALIBRATION_OFFSET       1500 // the adc offset in uV
 #define PID_POWER_LIMIT          35   // Sets the max pwm power limit
-#define POWER_LIMIT              25   // 30 watts default power limit
+#define POWER_LIMIT              32   // 30 watts default power limit
 #define I2C_SOFT_BUS_2           1
 #define HARDWARE_MAX_WATTAGE_X10 320
 #define OLED_I2CBB1              1
